df107b53148bbf38417b0ab0f080f09b378ceb95
MinaRins <minniemski@gmail.com>
2026-05-03 18:39:16 +0100

    update dht sensor to include blynk

--- dht_sensor.py
+++ dht_sensor.py
@@ -1,6 +1,10 @@
 import time
 import adafruit_dht
 import board
+from blynk import Blynk
+
+blynk = Blynk(BLYNK_AUTH)
+BLYNK_AUTH = "W_oWJ1QxAj7f_u-BDfvAynUFtdlkiK5z"
 
 dht = adafruit_dht.DHT11(board.D5)
 
