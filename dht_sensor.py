91bb79b46fbb93b7a8e47506d6d78c75f3a9acd5
MinaRins <minniemski@gmail.com>
2026-05-04 01:37:14 +0100

    added dht sensor to blynk web dashboard!

--- dht_sensor.py
+++ dht_sensor.py
@@ -1,26 +1,33 @@
 import time
+import requests
 import adafruit_dht
 import board
-from blynk import Blynk
 
-blynk = Blynk(BLYNK_AUTH)
-BLYNK_AUTH = "W_oWJ1QxAj7f_u-BDfvAynUFtdlkiK5z"
+BLYNK_TOKEN = "W_oWJ1QxAj7f_u-BDfvAynUFtdlkiK5z"
 
 dht = adafruit_dht.DHT11(board.D5)
+
+def send(pin, value):
+    url = f"https://blynk.cloud/external/api/update?token={BLYNK_TOKEN}&{pin}={value}"
+    try:
+        r = requests.get(url, timeout=5)
+        print("Sent:", pin, value, "| Response:", r.status_code)
+    except Exception as e:
+        print("Error:", e)
 
 while True:
     try:
         temperature = dht.temperature
         humidity = dht.humidity
 
-        print(f"Temp: {temperature:.1f}°C")
-        print(f"Humidity: {humidity:.1f}%")
+        if temperature is not None and humidity is not None:
+            print(f"Temp: {temperature:.1f}°C")
+            print(f"Humidity: {humidity:.1f}%")
+
+            send("v0", temperature)
+            send("v1", humidity)
 
     except RuntimeError:
         print("Read failed, retrying...")
 
     time.sleep(2)
-import adafruit_dht
-import board
-
-dht = adafruit_dht
